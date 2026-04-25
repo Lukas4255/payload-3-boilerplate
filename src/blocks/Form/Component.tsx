93059065239bba54af296f76be1df0583c036b88
Lukas4255 <rentreebv@gmail.com>
2026-04-25 23:50:44 +0200

    Update Component.tsx

--- src/blocks/Form/Component.tsx
+++ src/blocks/Form/Component.tsx
@@ -132,7 +132,7 @@ export const FormBlock: React.FC<
       <div className="p-4 lg:p-6 border border-border rounded-[0.8rem]">
         <FormProvider {...formMethods}>
           {!isLoading && hasSubmitted && confirmationType === 'message' && (
-            <RichText content={confirmationMessage} />
+            <RichText data={confirmationMessage} />
           )}
           {isLoading && !hasSubmitted && <p>Loading, please wait...</p>}
           {error && <div>{`${error.status || '500'}: ${error.message || ''}`}</div>}
