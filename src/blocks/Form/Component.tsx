6dc4ce32374f96f7a88629d06f5706de892d10d1
Lukas4255 <rentreebv@gmail.com>
2026-04-25 23:44:07 +0200

    Update Component.tsx

--- src/blocks/Form/Component.tsx
+++ src/blocks/Form/Component.tsx
@@ -128,7 +128,7 @@ export const FormBlock: React.FC<
   return (
     <div className="container lg:max-w-[48rem]">
       {enableIntro && introContent && !hasSubmitted && (
-        <RichText className="mb-8 lg:mb-12" content={introContent} enableGutter={false} />
+        <RichText className="mb-8 lg:mb-12" data={introContent} enableGutter={false} />
       )}
       <div className="p-4 lg:p-6 border border-border rounded-[0.8rem]">
         <FormProvider {...formMethods}>
